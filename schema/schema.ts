efd31005115f8dc4e4886e5f4aeae6fc6a349502
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-11 01:02:32 +0300

    added usertype schema

---
+++ schema/schema.ts
@@ -0,0 +1,10 @@
+import { GraphQLObjectType, GraphQLString } from "graphql";
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLString }
+  }
+});
