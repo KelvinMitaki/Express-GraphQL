62bf655876b793a7c1e3df1d8302b80b9f8a238a
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-11 01:20:35 +0300

    wired up schema

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import { schema } from "./schema/schema";
 
 const app = express();
 
 app.use(
   "/graphql",
-  // @ts-ignore
   graphqlHTTP({
-    graphiql: true
+    graphiql: true,
+    schema: schema
   })
 );
 
--- schema/schema.ts
+++ schema/schema.ts
@@ -1,4 +1,22 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+
+const users = [
+  {
+    id: "1",
+    firstName: "kevin",
+    age: 20
+  },
+  {
+    id: "2",
+    firstName: "brian",
+    age: 22
+  },
+  {
+    id: "3",
+    firstName: "wesley",
+    age: 26
+  }
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -8,3 +26,24 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLString }
   }
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parent, args, ctx, info) {
+        return users.find(usr => usr.id === args.id);
+      }
+    }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery
+});
+
+export { schema };
