8b7a955439c8457f4c96b652bc25e765cb2ae962
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-11 11:42:02 +0300

    added company type

--- schema/schema.ts
+++ schema/schema.ts
@@ -1,12 +1,30 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
 import axios from "axios";
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLString }
+    age: { type: GraphQLString },
+    company: {
+      type: CompanyType,
+      async resolve(parent, args) {
+        const res = await axios.get(
+          `http://localhost:3000/companies/${parent.companyId}`
+        );
+        return res.data;
+      }
+    }
   }
 });
 
